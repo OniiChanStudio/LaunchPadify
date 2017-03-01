cf428302dbaa181f5798be747bb270e89d8a8205
OniiChan <shanukmr197@gmail.com>
2017-03-01 17:05:02 +0530

    Cleanup

---
+++ app/src/main/java/com/oniichansblog/launchpadify/ImageFragment.java
@@ -0,0 +1,15 @@
+package com.oniichansblog.launchpadify;
+
+import android.app.Fragment;
+
+/**
+ * Created by skratch on 1/3/17.
+ */
+
+public class ImageFragment extends Fragment {
+
+    public ImageFragment(){
+    }
+
+
+}
--- app/src/main/java/com/oniichansblog/launchpadify/MainActivity.java
+++ app/src/main/java/com/oniichansblog/launchpadify/MainActivity.java
@@ -5,17 +5,14 @@ import android.view.View;
 import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
-    private MediaPlayer clap1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        clap1 = MediaPlayer.create(this, R.raw.clap1);
+
     }
 
-    public void clap1(View v){
-        clap1.start();
-    }
 
 }
